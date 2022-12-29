b3af0fc0de0b75bba05f254f3e9021699990935c
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2022-12-28 22:40:31 -0300

    mudando para CoffeCup

--- lib/src/presenter/ui/molecules/avaliation_select.dart
+++ lib/src/presenter/ui/molecules/avaliation_select.dart
@@ -1,22 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:manga_easy_advanced_search/src/presenter/ui/molecules/horns_rating.dart';
 
-class AvaliationSelect extends StatelessWidget {
+class AvaliationSelect extends StatefulWidget {
   const AvaliationSelect({super.key});
 
   @override
+  State<AvaliationSelect> createState() => _AvaliationSelectState();
+}
+
+class _AvaliationSelectState extends State<AvaliationSelect> {
+  int _value = 0;
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: const [
-        HornsRating(
-          hornClassification: 5,
-          ignoreGesture: true,
-          itemSize: 25,
+    return Column(
+      children: [
+        Row(
+          children: [
+            const HornsRating(
+              hornClassification: 5,
+              ignoreGesture: true,
+              itemSize: 25,
+            ),
+            const SizedBox(width: 10),
+            const Text('ou maior'),
+            Radio(
+              activeColor: Colors.black,
+              value: 1,
+              groupValue: _value,
+              onChanged: (int? value) {
+                setState(() {
+                  _value = value!;
+                });
+              },
+            )
+          ],
         ),
-        SizedBox(width: 10),
-        Text('ou maior')
       ],
-      
     );
   }
 }
