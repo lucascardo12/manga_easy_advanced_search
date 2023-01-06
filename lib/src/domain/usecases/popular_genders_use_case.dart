dc1fdecc98e9d9e8839b3df12e4af209eae836cd
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-01-06 15:53:00 -0300

    trocando generos favoritos

--- lib/src/domain/usecases/popular_genders_use_case.dart
+++ lib/src/domain/usecases/popular_genders_use_case.dart
@@ -1,7 +1,7 @@
 import 'package:manga_easy_sdk/manga_easy_sdk.dart';
 
 class GetPopularGenderCase {
-  final _list = ['açao', 'artes-marciais', 'comedia', 'drama', 'romance'];
+  final _list = ['açao', 'comedia', 'drama', 'romance', 'shounen'];
   List<GenerosModel> call() {
     return _list
         .map((e) => GenerosModel.carregaGeneros(codigo: e).first)
