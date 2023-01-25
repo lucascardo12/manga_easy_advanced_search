22dfff5cfb1716a8fb33c377959c052259a5b0e0
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-01-25 01:16:02 -0300

    Adding Api in MangaInfoSearch

--- lib/src/presenter/ui/pages/search_page.dart
+++ lib/src/presenter/ui/pages/search_page.dart
@@ -1,7 +1,12 @@
 import 'package:coffee_cup/coffe_cup.dart';
 import 'package:coffee_cup/features/buttons/coffee_switch_button.dart';
 import 'package:flutter/material.dart';
+import 'package:manga_easy_advanced_search/src/data/models/manga_model.dart';
+import 'package:manga_easy_advanced_search/src/data/repositories/manga_repository.dart';
+import 'package:manga_easy_advanced_search/src/data/repositories/manga_repository_imp.dart';
+import 'package:manga_easy_advanced_search/src/data/service/dio_service.dart';
 import 'package:manga_easy_advanced_search/src/domain/usecases/popular_genders_use_case.dart';
+import 'package:manga_easy_advanced_search/src/presenter/controllers/manga_controller.dart';
 import 'package:manga_easy_advanced_search/src/presenter/controllers/search_controller.dart';
 import 'package:manga_easy_advanced_search/src/presenter/ui/organisms/manga_info_search.dart';
 import 'package:manga_easy_advanced_search/src/presenter/ui/molecules/text_field_search.dart';
@@ -17,7 +22,14 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
-  final ct = SearchController(GetPopularGenderCase());
+  final ct = GenderController(GetPopularGenderCase());
+  final MangaController _controller =
+      MangaController(MangaRepositoryImp(DioService()));
+  @override
+  void initState() {
+    _controller.fetch();
+    super.initState();
+  }
 
   bool selectButton = true;
   @override
@@ -25,21 +37,44 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       backgroundColor: ThemeService.of.backgroundColor,
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            FieldTextSearch(ct: ct),
-            CoffeeSwitchButton(
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              snap: false,
+              pinned: false,
+              floating: false,
+              title: FieldTextSearch(ct: ct),
+              shadowColor: Colors.transparent,
+              backgroundColor: Colors.transparent,
+            ),
+            //
+            SliverToBoxAdapter(
+              child: CoffeeSwitchButton(
                 value: selectButton,
                 onChanged: (_) {
                   setState(() {
                     selectButton = !selectButton;
                   });
-                }),
-            selectButton
-                ? const MangaInfoSearch()
-                : const Expanded(child: MangaInfoSearchGreadView()),
-            CoffeeField(),
+                },
+              ),
+            ),
+            SliverToBoxAdapter(
+                child: ValueListenableBuilder<List<Mangas>?>(
+              valueListenable: _controller.mangas,
+              builder: (_, manga, __) {
+                return manga != null
+                    ? selectButton
+                        ? ListView.builder(
+                            physics: const NeverScrollableScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: manga.length,
+                            itemBuilder: (_, idx) =>
+                                MangaInfoSearch(data: manga[idx]),
+                          )
+                        : const Expanded(child: MangaInfoSearchGreadView())
+                    : Container();
+              },
+            ))
           ],
         ),
       ),
