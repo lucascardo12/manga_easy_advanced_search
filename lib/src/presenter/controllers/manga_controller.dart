fe6c3f784d0d0311a543a866340eabed0a9ca86f
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-02-21 20:09:09 -0300

    join the controller

--- lib/src/presenter/controllers/manga_controller.dart
+++ lib/src/presenter/controllers/manga_controller.dart
@@ -1,16 +1,22 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:manga_easy_advanced_search/src/domain/entities/manga_filter_entity.dart';
+import 'package:manga_easy_advanced_search/src/domain/repositories/manga_repository.dart';
+import 'package:manga_easy_advanced_search/src/domain/usecases/get_popular_genres_use_case.dart';
 import 'package:manga_easy_sdk/manga_easy_sdk.dart';
-import '../../domain/repositories/manga_repository.dart';
 
 class MangaController {
   final MangaRepository _mangaRepository;
-  MangaController(this._mangaRepository);
+  final GetPopularGenresUseCase getPopularGenderCase;
+  MangaController(this._mangaRepository, this.getPopularGenderCase) {
+    fetch();
+  }
+
+  MangaFilterEntity mangaFilter = MangaFilterEntity();
 
   final ValueNotifier<List<InfoComicModel>?> mangas =
       ValueNotifier<List<InfoComicModel>?>(null);
 
   fetch() async {
-    //TODO: implementar filtro e paginação
-    mangas.value = await _mangaRepository.getManga();
+    mangas.value = await _mangaRepository.getManga(filter: mangaFilter);
   }
 }
--- lib/src/presenter/controllers/search_controller.dart
+++
@@ -1,7 +0,0 @@
-import 'package:manga_easy_advanced_search/src/domain/usecases/popular_genders_use_case.dart';
-
-class GenderController {
-  final GetPopularGenderCase getPopularGenderCase;
-
-  GenderController(this.getPopularGenderCase);
-}
