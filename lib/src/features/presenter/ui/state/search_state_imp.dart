34b80fe7b6d885b220881273c485b5d7e5b2a460
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-03-04 14:22:03 -0300

    Solid principles

---
+++ lib/src/features/presenter/ui/state/search_state_imp.dart
@@ -0,0 +1,24 @@
+import 'package:manga_easy_advanced_search/src/features/presenter/ui/state/search_state.dart';
+import 'package:manga_easy_sdk/manga_easy_sdk.dart';
+
+class SearchInitialState extends SearchState {}
+
+class SearchLoadingState extends SearchState {}
+
+class SearchErrorState extends SearchState {
+  final String message;
+
+  SearchErrorState(this.message);
+}
+
+class SearchNotfoundState extends SearchState {
+  final String message;
+
+  SearchNotfoundState(this.message);
+}
+
+class SearchDoneState extends SearchState {
+  final List<InfoComicModel> mangas;
+
+  SearchDoneState(this.mangas);
+}
