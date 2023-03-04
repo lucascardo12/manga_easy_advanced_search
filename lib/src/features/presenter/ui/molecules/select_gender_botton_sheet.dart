65525c658bb28c40017d132465e73f6916221683
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-03-04 15:37:38 -0300

    update state botton sheet

--- lib/src/features/presenter/ui/molecules/select_gender_botton_sheet.dart
+++ lib/src/features/presenter/ui/molecules/select_gender_botton_sheet.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 import 'package:manga_easy_advanced_search/src/features/presenter/controllers/manga_controller.dart';
 import 'package:manga_easy_advanced_search/src/features/presenter/ui/molecules/category_select_botton_sheet.dart';
 import 'package:manga_easy_advanced_search/src/features/presenter/ui/organisms/see_more_text_button.dart';
+import 'package:manga_easy_themes/manga_easy_themes.dart';
 
-class SelectGenderBottonSheet extends StatelessWidget {
+class SelectGenderBottonSheet extends StatefulWidget {
   final MangaController ct;
   const SelectGenderBottonSheet({super.key, required this.ct});
 
+  @override
+  State<SelectGenderBottonSheet> createState() =>
+      _SelectGenderBottonSheetState();
+}
+
+class _SelectGenderBottonSheetState extends State<SelectGenderBottonSheet> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -18,19 +25,40 @@ class SelectGenderBottonSheet extends StatelessWidget {
             const CoffeeText(
               text: 'Gêneros',
             ),
-            SeeMoreTextButton(ct: ct)
+            TextButton.icon(
+              onPressed: () {
+                showModalBottomSheet<void>(
+                  context: context,
+                  backgroundColor: ThemeService.of.backgroundColor,
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20)),
+                  ),
+                  builder: (BuildContext context) => AllGenders(ct: widget.ct),
+                ).then((value) {
+                  setState(() {});
+                });
+              },
+              icon: const CoffeeText(
+                  text: 'Ver mais', typography: CoffeeTypography.body),
+              label: const Icon(
+                Icons.arrow_forward_ios_rounded,
+                size: 14,
+              ),
+            ),
           ],
         ),
         SizedBox(
           height: 40,
           child: ListView.builder(
             scrollDirection: Axis.horizontal,
-            itemCount: ct.getPopularGenderCase().length,
+            itemCount: widget.ct.getPopularGenderCase().length,
             itemBuilder: (context, index) {
-              final gender = ct.getPopularGenderCase()[index];
+              final gender = widget.ct.getPopularGenderCase()[index];
               return CategorySelectBottonSheet(
                 gender: gender,
-                filterGenders: ct.mangaFilter.genders,
+                filterGenders: widget.ct.mangaFilter.genders,
               );
             },
           ),
--- lib/src/features/presenter/ui/organisms/see_more_text_button.dart
+++ lib/src/features/presenter/ui/organisms/see_more_text_button.dart
@@ -3,70 +3,49 @@ import 'package:flutter/material.dart';
 import 'package:manga_easy_advanced_search/src/features/presenter/controllers/manga_controller.dart';
 import 'package:manga_easy_advanced_search/src/features/presenter/ui/molecules/category_select_botton_sheet.dart';
 import 'package:manga_easy_sdk/manga_easy_sdk.dart';
-import 'package:manga_easy_themes/manga_easy_themes.dart';
 
-class SeeMoreTextButton extends StatelessWidget {
+class AllGenders extends StatelessWidget {
   final MangaController ct;
-  const SeeMoreTextButton({super.key, required this.ct});
+  const AllGenders({super.key, required this.ct});
 
   @override
   Widget build(BuildContext context) {
-    return TextButton.icon(
-      onPressed: () {
-        showModalBottomSheet<void>(
-            context: context,
-            backgroundColor: ThemeService.of.backgroundColor,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-            ),
-            builder: (BuildContext context) {
-              return Container(
-                height: MediaQuery.of(context).size.height * 0.5,
-                padding: const EdgeInsets.all(5),
-                child: Column(
-                  children: [
-                    const SizedBox(height: 15),
-                    const CoffeeText(
-                        text: 'Selecionar gêneros',
-                        typography: CoffeeTypography.title),
-                    const SizedBox(height: 15),
-                    Expanded(
-                      flex: 4,
-                      child: ListView(
-                        scrollDirection: Axis.vertical,
-                        children: [
-                          Wrap(
-                            runSpacing: 4,
-                            alignment: WrapAlignment.center,
-                            runAlignment: WrapAlignment.center,
-                            crossAxisAlignment: WrapCrossAlignment.center,
-                            spacing: 2,
-                            children: GenerosModel.carregaGeneros()
-                                .map((e) => CategorySelectBottonSheet(
-                                      gender: e,
-                                      filterGenders: ct.mangaFilter.genders,
-                                    ))
-                                .toList(),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(height: 6),
-                    CoffeeButton(
-                      label: 'Aplicar',
-                      onPress: () => Navigator.pop(context),
-                    )
-                  ],
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.5,
+      padding: const EdgeInsets.all(5),
+      child: Column(
+        children: [
+          const SizedBox(height: 15),
+          const CoffeeText(
+              text: 'Selecionar gêneros', typography: CoffeeTypography.title),
+          const SizedBox(height: 15),
+          Expanded(
+            flex: 4,
+            child: ListView(
+              scrollDirection: Axis.vertical,
+              children: [
+                Wrap(
+                  runSpacing: 4,
+                  alignment: WrapAlignment.center,
+                  runAlignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.center,
+                  spacing: 2,
+                  children: GenerosModel.carregaGeneros()
+                      .map((e) => CategorySelectBottonSheet(
+                            gender: e,
+                            filterGenders: ct.mangaFilter.genders,
+                          ))
+                      .toList(),
                 ),
-              );
-            });
-      },
-      icon:
-          const CoffeeText(text: 'Ver mais', typography: CoffeeTypography.body),
-      label: const Icon(
-        Icons.arrow_forward_ios_rounded,
-        size: 14,
+              ],
+            ),
+          ),
+          const SizedBox(height: 6),
+          CoffeeButton(
+            label: 'Aplicar',
+            onPress: () => Navigator.pop(context),
+          )
+        ],
       ),
     );
   }
