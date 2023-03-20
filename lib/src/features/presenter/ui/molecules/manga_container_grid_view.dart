0d5670cb5f1d9352e26d88acafbbd14de8e2a424
lucas <lucas.cardo123@hotmail.com>
2023-03-20 17:33:23 -0400

    Add route migrate in gridview manga

--- lib/src/features/presenter/ui/molecules/manga_container_grid_view.dart
+++ lib/src/features/presenter/ui/molecules/manga_container_grid_view.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_cup/coffe_cup.dart';
 import 'package:flutter/material.dart';
+import 'package:manga_easy_advanced_search/src/microapp/routes/external_routes.dart';
 import 'package:manga_easy_sdk/manga_easy_sdk.dart';
 
 class MangaContainerGridView extends StatelessWidget {
@@ -8,28 +9,38 @@ class MangaContainerGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        const SizedBox(height: 5),
-        CoffeeMangaCover(
-          filtraImg: true,
-          cover: data.thumb,
-          height: 200,
-          width: 125,
-          headers: Global.header,
-        ),
-        SizedBox(
-          height: 30,
-          child: Center(
-            child: CoffeeText(
-              text: data.name,
-              maxLines: 2,
-              overflow: TextOverflow.ellipsis,
-            ),
+    return InkWell(
+      onTap: () => Navigator.pushNamed(
+        context,
+        ExternalRoutes.migrate.url,
+        arguments: {
+          "nameManga": data.name,
+          "isUniqueId": true,
+        },
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          const SizedBox(height: 5),
+          CoffeeMangaCover(
+            filtraImg: true,
+            cover: data.thumb,
+            height: 200,
+            width: 125,
+            headers: Global.header,
           ),
-        )
-      ],
+          SizedBox(
+            height: 30,
+            child: Center(
+              child: CoffeeText(
+                text: data.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
