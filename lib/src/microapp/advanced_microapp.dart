fb614fb938d1b830057065a85c47569e3a319f60
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-03-26 00:21:48 -0300

    test

--- lib/src/microapp/advanced_microapp.dart
+++ lib/src/microapp/advanced_microapp.dart
@@ -3,7 +3,7 @@ import 'package:get_it/get_it.dart';
 import 'package:manga_easy_advanced_search/src/features/data/datasources/manga_datasource.dart';
 import 'package:manga_easy_advanced_search/src/features/data/datasources/manga_datasource_v1.dart';
 import 'package:manga_easy_advanced_search/src/features/data/repositories/manga_repository_imp.dart';
-import 'package:manga_easy_advanced_search/src/features/data/service/service_pref_imp.dart';
+import 'package:manga_easy_advanced_search/src/features/data/services/service_pref_imp.dart';
 import 'package:manga_easy_advanced_search/src/features/domain/mappers/mapper_manga.dart';
 import 'package:manga_easy_advanced_search/src/features/domain/repositories/manga_repository.dart';
 import 'package:manga_easy_advanced_search/src/features/domain/service/service_pref.dart';
