25769536c3425fbb1ba51174a7202949fd2204fb
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-06-22 23:31:09 -0300

    remove ServicePrefs

--- lib/src/microapp/advanced_microapp.dart
+++ lib/src/microapp/advanced_microapp.dart
@@ -3,10 +3,8 @@ import 'package:get_it/get_it.dart';
 import 'package:manga_easy_advanced_search/src/features/data/datasources/manga_datasource.dart';
 import 'package:manga_easy_advanced_search/src/features/data/datasources/manga_datasource_v1.dart';
 import 'package:manga_easy_advanced_search/src/features/data/repositories/manga_repository_imp.dart';
-import 'package:manga_easy_advanced_search/src/features/data/services/service_pref_imp.dart';
 import 'package:manga_easy_advanced_search/src/features/domain/mappers/mapper_manga.dart';
 import 'package:manga_easy_advanced_search/src/features/domain/repositories/manga_repository.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/service/service_pref.dart';
 import 'package:manga_easy_advanced_search/src/features/domain/usecases/get_popular_genres_use_case.dart';
 import 'package:manga_easy_advanced_search/src/features/domain/usecases/get_popular_genres_use_case_imp.dart';
 import 'package:manga_easy_advanced_search/src/features/presenter/ui/pages/search_page.dart';
@@ -22,8 +20,6 @@ class AdvancedMicroApp extends MicroApp {
   void registerDependencies() {
     //mappers
     getIt.registerFactory(() => MapperManga());
-    //service
-    getIt.registerFactory<ServicePrefs>(() => ServicePrefsImp());
 
     //datasource
     getIt.registerFactory<MangaDatasource>(() => MangaDatasourceV1(getIt()));
