ad9123ef8106c85f1e3321db30bcbc2ca4d87e4d
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-07-20 23:00:11 -0300

    update repository

--- lib/src/features/data/repositories/manga_repository_imp.dart
+++ lib/src/features/data/repositories/manga_repository_imp.dart
@@ -1,22 +1,55 @@
-import 'package:manga_easy_advanced_search/src/features/data/datasources/manga_datasource.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/entities/manga_filter_entity.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/mappers/mapper_manga.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/repositories/manga_repository.dart';
+import 'package:advanced_search/src/features/domain/entities/manga_filter_entity.dart';
+import 'package:advanced_search/src/features/domain/mappers/mapper_manga.dart';
+import 'package:advanced_search/src/features/domain/repositories/manga_repository.dart';
+import 'package:client_driver/client_driver.dart';
 import 'package:manga_easy_sdk/manga_easy_sdk.dart';
 
 class MangaRepositoryImp implements MangaRepository {
-  final MangaDatasource _mangaDatasource;
   final MapperManga _mapperManga;
-  MangaRepositoryImp(this._mangaDatasource, this._mapperManga);
+  final ClientRequest _clientDriver;
+  final ApiResponseParser _apiResponseParser;
+
+  MangaRepositoryImp(
+    this._mapperManga,
+    this._clientDriver,
+    this._apiResponseParser,
+  );
+  final String path = 'catalog';
+  final String url = 'http://catalog.lucas-cm.com.br';
+  final String version = 'v1';
 
   @override
   Future<List<InfoComicModel>> getManga({
     required MangaFilterEntity filter,
-    int? limit,
     int? offset,
+    int? limit,
   }) async {
-    var result = await _mangaDatasource.list(
-        filter: filter, limit: limit, offset: offset);
+    var params = '';
+    if (filter.search != null) {
+      params += 'search=${filter.search!.trim()}&';
+    }
+    if (filter.genders.isNotEmpty) {
+      params += 'genders=${filter.genders.join('<>')}&';
+    }
+
+    if (filter.yearAt != null && filter.yearFrom != null) {
+      params += 'yearFrom=${filter.yearFrom}&';
+      params += 'yearAt=${filter.yearAt}&';
+    }
+
+    if (filter.author != null) {
+      params += 'author=${filter.author}&';
+    }
+    if (Global.filterContentOver18) {
+      params += 'isAdult=${Global.filterContentOver18}&';
+    }
+    var response = await _clientDriver.get(
+      path: '$url/$version/$path?limit=$limit&offset=$offset&$params',
+    );
+
+    var result = _apiResponseParser.handleResponse(
+        statusCode: response.statusCode, response: response.data);
+
     return result.data.map((e) => _mapperManga.from(e)).toList();
   }
 
