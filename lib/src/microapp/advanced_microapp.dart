8089af67529c93291469845ba567b58ed27ad753
Bruno Almeida de Matos <brunoalmeidadem@gmail.com>
2023-07-20 23:00:02 -0300

    remove datasource, update repository

--- lib/src/microapp/advanced_microapp.dart
+++ lib/src/microapp/advanced_microapp.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:manga_easy_advanced_search/src/features/data/datasources/manga_datasource.dart';
-import 'package:manga_easy_advanced_search/src/features/data/datasources/manga_datasource_v1.dart';
-import 'package:manga_easy_advanced_search/src/features/data/repositories/manga_repository_imp.dart';
-import 'package:manga_easy_advanced_search/src/features/data/repositories/search_repository_imp.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/mappers/mapper_manga.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/repositories/manga_repository.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/repositories/search_repository.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/usecases/get_popular_genres_use_case.dart';
-import 'package:manga_easy_advanced_search/src/features/domain/usecases/get_popular_genres_use_case_imp.dart';
-import 'package:manga_easy_advanced_search/src/features/presenter/ui/pages/search_page.dart';
+import 'package:advanced_search/src/features/data/repositories/manga_repository_imp.dart';
+import 'package:advanced_search/src/features/data/repositories/search_repository_imp.dart';
+import 'package:advanced_search/src/features/domain/mappers/mapper_manga.dart';
+import 'package:advanced_search/src/features/domain/repositories/manga_repository.dart';
+import 'package:advanced_search/src/features/domain/repositories/search_repository.dart';
+import 'package:advanced_search/src/features/domain/usecases/get_popular_genres_use_case.dart';
+import 'package:advanced_search/src/features/domain/usecases/get_popular_genres_use_case_imp.dart';
+import 'package:advanced_search/src/features/presenter/ui/pages/search_page.dart';
 import 'package:manga_easy_routes/manga_easy_routes.dart';
-import 'package:manga_easy_advanced_search/src/features/presenter/controllers/manga_controller.dart';
+import 'package:advanced_search/src/features/presenter/controllers/manga_controller.dart';
 
 class AdvancedMicroApp extends MicroApp {
   GetIt getIt = GetIt.instance;
@@ -23,12 +21,9 @@ class AdvancedMicroApp extends MicroApp {
     //mappers
     getIt.registerFactory(() => MapperManga());
 
-    //datasource
-    getIt.registerFactory<MangaDatasource>(() => MangaDatasourceV1(getIt()));
-
     //repositories
     getIt.registerFactory<MangaRepository>(
-      () => MangaRepositoryImp(getIt(), getIt()),
+      () => MangaRepositoryImp(getIt(), getIt(), getIt()),
     );
     getIt.registerFactory<SearchHistoryRepository>(
       () => SearchHistoryRepositoryImp(getIt()),
